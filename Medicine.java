e070ee9eaa004fb9081a44cc7af9a85dba52faf7
Muhammad Abdullah Usman <Abdullah.usw474@gmail.com>
2025-05-23 14:04:09 +0500

    Add files via upload

---
+++ Medicine.java
@@ -0,0 +1,66 @@
+public class Medicine {
+    private static int medicineID = 0;
+    private String medicineName;
+    private String medicineType;
+    private double price;
+    private int qtyAvailable;
+    private String isAvailable;
+
+    public Medicine(String medicineName, String medicineType, double price, int qtyAvailable, String isAvailable) {
+        medicineID++;
+        this.medicineName = medicineName;
+        this.medicineType = medicineType;
+        this.price = price;
+        this.qtyAvailable = qtyAvailable;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getMedicineID() {
+        return medicineID;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public String getMedicineType() {
+        return medicineType;
+    }
+
+    public void setMedicineType(String medicineType) {
+        this.medicineType = medicineType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQtyAvailable() {
+        return qtyAvailable;
+    }
+
+    public void setQtyAvailable(int qtyAvailable) {
+        this.qtyAvailable = qtyAvailable;
+    }
+
+    public String getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(String isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String toString() {
+        return "Medicine ID: " + medicineID + ", Name: " + medicineName + ", Type: " + medicineType +
+                ", Price: " + price + ", Qty: " + qtyAvailable + ", Availability: " + isAvailable;
+    }
+}
